2708e36621c4689e3018279f1f6f0e24fa41ade0
Udesh-Ranjan <ranjanudesh404@gmail.com>
2020-11-08 17:35:02 +0530

    Main file

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stdlib.h>
+#include "d:/code/c++/open/3d-Rotating-Cube/Dependencie/freeglut/include/GL/freeglut.h"
+using namespace std;
+
+
+/////Main///////
+int main(){
+    
+    return 0;
+} 
